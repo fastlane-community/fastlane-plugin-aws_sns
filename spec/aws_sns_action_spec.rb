402209a2ebda0e6a6d9af2caae561f992ebc609f
Marc PEREZ <marc.perez@epitech.eu>
2021-07-14 16:09:05 +0200

    Modernized the smoke test

--- spec/aws_sns_action_spec.rb
+++ spec/aws_sns_action_spec.rb
@@ -1,18 +1,24 @@
-describe Fastlane::Actions::AwsSnsAction do
-  describe '#run' do
-    it 'doesn\'t smoke when you run it' do
-      expect(Fastlane::UI).to receive(:important).with('Created String')
+describe Fastlane do
+  describe Fastlane::FastFile do
+    describe 'aws_sns' do
+      it 'returns the FCM Platform ARN' do
+        expect(Fastlane::UI).to receive(:important).with('Created String')
+        result = Fastlane::FastFile.new.parse("
+        lane :test do
+          aws_sns_client = Aws::SNS::Client.new(stub_responses: true)
 
-      aws_sns_client = Aws::SNS::Client.new(stub_responses: true)
+          arn = aws_sns({
+            aws_sns_client: aws_sns_client,
+            platform: 'FCM',
+            platform_name: 'FCM_SNS_PLATFORM_APP',
+            platform_fcm_server_key: 'PLACEHOLDER_FCM_KEY',
+            attributes_override: {}
+          })
+        end
+        ").runner.execute(:test)
 
-      arn = Fastlane::Actions::AwsSnsAction.run({
-        aws_sns_client: aws_sns_client,
-        platform: 'FCM',
-        platform_name: 'FCM_SNS_PLATFORM_APP',
-        platform_fcm_server_key: 'PLACEHOLDER_FCM_KEY',
-        attributes_override: {}
-      })
-      expect(arn).to eq('String')
+        expect(result).to eq("String")
+      end
     end
   end
 end
